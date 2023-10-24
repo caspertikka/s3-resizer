5a262cdb04dd1480eadd9d05ec6c09b05e7885c0
caspertikka <casper@vevex.fi>
2023-10-24 14:07:37 +0300

    Update

--- index.js
+++ index.js
@@ -1,79 +1,30 @@
-import {S3Client, GetObjectCommand} from "@aws-sdk/client-s3";
-import {Upload} from '@aws-sdk/lib-storage';
+import { S3Client, GetObjectCommand } from "@aws-sdk/client-s3";
+import { Upload } from '@aws-sdk/lib-storage';
 import Sharp from 'sharp';
-
-const PathPattern = /(.*\/)?(.*)\/(.*)/;
-
-// parameters
-const {BUCKET, URL} = process.env;
-const WHITELIST = process.env.WHITELIST
-    ? Object.freeze(process.env.WHITELIST.split(' '))
-    : null;
 
 const s3Client = new S3Client();
 
 export const handler = async (event) => {
-    const path = event.queryStringParameters?.path;
-    if (!path) {
-        return errorResponse('Parameter "?path=" is empty or missing');
-    }
-
-    const parts = PathPattern.exec(path);
-    if (!parts || !parts[2] || !parts[3]) {
-        return errorResponse('Parameter "?path=" should look like: path/150x150_max/image.jpg');
-    }
-    const dir = parts[1] || '';
-    const resizeOption = parts[2];  // e.g. "150x150_max"
-    const sizeAndAction = resizeOption.split('_');
-    const filename = parts[3];
-
-    const sizes = sizeAndAction[0].split("x");
-    const action = sizeAndAction.length > 1 ? sizeAndAction[1] : null;
-
-    // Whitelist validation.
-    if (WHITELIST && !WHITELIST.includes(resizeOption)) {
-        return errorResponse(`WHITELIST is set but does not contain the size parameter "${resizeOption}"`, 403);
-    }
-
-    // Action validation.
-    if (action && action !== 'max' && action !== 'min') {
-        return errorResponse(
-            `Unknown func parameter "${action}"\n` +
-            'For query ".../150x150_func", "_func" must be either empty, "_min" or "_max"'
-        );
-    }
-
     try {
+        const { bucket, object } = event.Records[0].s3;
+
         const originImage = await s3Client.send(new GetObjectCommand({
-            Bucket: BUCKET,
-            Key: dir + filename,
+            Bucket: bucket.name,
+            Key: object.key,
         }));
 
-        const width = sizes[0] === 'AUTO' ? null : parseInt(sizes[0]);
-        const height = sizes[1] === 'AUTO' ? null : parseInt(sizes[1]);
-        let fit;
-        switch (action) {
-            case 'max':
-                fit = 'inside';
-                break;
-            case 'min':
-                fit = 'outside';
-                break;
-            default:
-                fit = 'cover';
-                break;
-        }
-        const sharp = Sharp({failOn: 'none'})
-            .resize(width, height, {withoutEnlargement: true, fit})
-            .rotate();
+        const sharp = Sharp({ failOn: 'none' })
+            .resize(500, null, { withoutEnlargement: true })
+
+        const newKey = `500/${object.key}`
 
         // This does not work: await s3Client.send(new PutObjectCommand({params})
         // Solution: https://github.com/aws/aws-sdk-js-v3/issues/1920#issuecomment-761298908
         const upload = new Upload({
             client: s3Client,
             params: {
-                Bucket: BUCKET,
-                Key: path,
+                Bucket: process.env.BUCKET,
+                Key: newKey,
                 Body: originImage.Body.pipe(sharp),
                 ContentType: originImage.ContentType,
                 CacheControl: 'public, max-age=86400'
@@ -82,8 +33,7 @@ export const handler = async (event) => {
         await upload.done();
 
         return {
-            statusCode: 301,
-            headers: {"Location" : `${URL}/${path}`}
+            statusCode: 200,
         };
     } catch (e) {
         return errorResponse('Exception: ' + e.message, e.statusCode || 400);
